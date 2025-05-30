835e6f8c43244f06b261a9f8988b146c22a6f349
Syed Madni Ashraf <syedmadniashraf.offc@gmailcom>
2025-05-30 23:31:29 +0530

    Chapter2 complete

---
+++ chapter2/05_type.py
@@ -0,0 +1,4 @@
+a = "31.5"
+b = float(a)  # a but the type should be float
+t = type(b) 
+print(t)
---
+++ chapter2/06_input.py
@@ -0,0 +1,6 @@
+a = int(input("Enter number 1: "))
+b = int(input("ENter number 2: "))
+
+print("Number a is :",a)
+print("Number b is :",b)
+print("sum is", a + b)
