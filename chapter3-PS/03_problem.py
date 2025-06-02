3ef015cb5e157460297cec1e245a2b9c120ddeb6
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-02 23:30:23 +0530

    Chapter3 PS

---
+++ chapter3-PS/03_problem.py
@@ -0,0 +1,5 @@
+# 3. Write a program to detect double space in a string.
+
+name = "Madni is a  good boy"
+
+print(name.find("  "))
---
+++ chapter3-PS/04_problem.py
@@ -0,0 +1,5 @@
+# 4. Replace the double space from problem 3 with single spaces.
+
+name = "Madni is a  good  boy"
+
+print(name.replace("  "," "))# Strings are immutable which means you cannot change them by running functions on them.
