73f0fa9f1a3c7da5fdbaefdee49559010758a4c3
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-04 23:14:15 +0530

    chapter 5 completed

---
+++ chapter5/01_dict.py
@@ -0,0 +1,9 @@
+# Dictionary is a collection of keys-value pairs.
+
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+# print(marks, type(marks))
+print(marks["Harry"])
---
+++ chapter5/03_sets.py
@@ -0,0 +1,8 @@
+# Set is a collection of non-repetitive elements.
+
+e = set()  # Empty set.
+
+s = {1, 5, 32, 5, 5}  # Dont use s = {} as it will create an empty dictionary.
+
+print(s)
+
