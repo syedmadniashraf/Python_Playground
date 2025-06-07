6944c7b9b90a2be4dcc48850435adc9ba760ce00
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-07 23:33:32 +0530

    chapter6

---
+++ chapter6/01_conditionals.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age: "))
+
+if(a>=18):
+    print("You are above the age of concent")
+    print("Good for you")
+
+else:
+    print("You are below the age of concent")
+    
