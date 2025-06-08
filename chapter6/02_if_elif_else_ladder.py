98c645c5d01f0b6c9410691105ef770d597a63f9
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-08 23:33:02 +0530

    chapter6 completed

--- chapter6/02_if_elif_else_ladder.py
+++ chapter6/02_if_elif_else_ladder.py
@@ -1,10 +1,16 @@
 a = int(input("Enter your age: "))
 
-# If Else ladder
+# If Elif Else ladder
 
 if(a>=18):
     print("You are above the age of concent")
     print("Good for you")
+
+elif(a<0):
+    print("You are entering an invalid negative age")
+
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
 
 else:
     print("You are below the age of concent")
---
+++ chapter6/03_multiple_if_statement.py
@@ -0,0 +1,27 @@
+a = int(input("Enter your age: "))
+
+# If statement no: 1
+if(a%2==0):
+    print("a is even")
+# End of If statement no: 1
+
+
+
+# If statement no: 2
+if(a>=18):
+    print("You are above the age of concent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering an invalid negative age")
+
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
+
+else:
+    print("You are below the age of concent")
+# End of If statement no: 2
+
+
+print("End of program")
+    
