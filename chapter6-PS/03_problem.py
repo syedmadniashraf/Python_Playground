471afc39766057244b3d8ac576fad076804bd547
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-10 22:32:54 +0530

    ch-6 modified

---
+++ chapter6-PS/03_problem.py
@@ -0,0 +1,3 @@
+# 3. A spam comment is defined as a text containing following keywords:
+# “Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program
+# to detect these spams.
