962de26e094e76c43982afddcb4c7308a40447ce
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-10 22:10:57 +0530

    Ch 6 ps

---
+++ chapter6-PS/02_problem.py
@@ -0,0 +1,3 @@
+# 2. Write a program to find out whether a student has passed or failed if it requires a
+# total of 40% and at least 33% in each subject to pass. Assume 3 subjects and
+# take marks as an input from the user.
