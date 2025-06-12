abb9c94c291c53d62e669ab9467b41c6af499024
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-12 23:26:26 +0530

    Ch 6 ps

--- chapter6-PS/02_problem.py
+++ chapter6-PS/02_problem.py
@@ -1,3 +1,20 @@
 # 2. Write a program to find out whether a student has passed or failed if it requires a
 # total of 40% and at least 33% in each subject to pass. Assume 3 subjects and
 # take marks as an input from the user.
+
+
+marks1 = int(input("Enter Marks 1: "))
+marks2 = int(input("Enter Marks 2: "))
+marks3 = int(input("Enter Marks 3: "))
+
+
+# check for total percenrtage
+
+total_percentage = (100*(marks1 + marks2 + marks3))/300
+
+if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+    print("You are pass!",total_percentage)
+
+else:
+    print("You failed, Try again next year!",total_percentage)
+
