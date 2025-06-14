621971acf82fe97ceda53aca09ac030555c90436
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-14 23:17:41 +0530

    ch6 ps

---
+++ chapter6-PS/06_problem.py
@@ -0,0 +1,9 @@
+# 6. Write a program to calculate the grade of a student from his marks from the
+# following scheme:
+
+# 90 – 100 => Ex
+# 80 – 90 => A
+# 70 – 80 => B
+# 60 – 70 =>C
+# 50 – 60 => D
+# <50 => F
