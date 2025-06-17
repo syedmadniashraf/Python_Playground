515841db5402302d0365ad28e248328cdb17e075
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-17 22:44:17 +0530

    chapter7

---
+++ chapter7/03_list_using_while.py
@@ -0,0 +1,7 @@
+# Quick Quiz: Write a program to print the content of a list using while loops.
+
+l = [1,"Harry", False, "This", "Rohan", "Shubham", "Shubhi"]
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i += 1
---
+++ chapter7/quick_quiz.py
@@ -0,0 +1,8 @@
+# Quick Quiz: Write a program to print 1 to 50 using a while loop.
+
+i = 1
+
+while(i<51):
+    print(i)
+    i += 1
+    
