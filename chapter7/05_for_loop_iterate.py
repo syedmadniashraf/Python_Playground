f13369ce6f41204be6ff3c89c23781b8fd2ed82a
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-22 19:12:06 +0530

    chapter 7 completed

---
+++ chapter7/05_for_loop_iterate.py
@@ -0,0 +1,17 @@
+# For Loop with Lists
+l = [1, 4, 6, 234, 6, 764]
+
+for i in l:
+    print(i) 
+
+# For Loop with Tuple
+t = (1, 22, 334, 44)
+
+for i in t:
+    print(i) 
+
+# For Loop with String
+s = "Harry"
+
+for i in s:
+    print(i)
