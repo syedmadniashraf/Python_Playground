7d7e54705e9311b906ea25c9d94b88c822727888
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-26 22:58:07 +0530

    ch8

---
+++ chapter8/01_functions.py
@@ -0,0 +1,25 @@
+# a = int(input("Enter number: "))
+# b = int(input("Enter number: "))
+# c = int(input("Enter number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# a = int(input("Enter number: "))
+# b = int(input("Enter number: "))
+# c = int(input("Enter number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+def avg():
+    a = int(input("Enter number: "))
+    b = int(input("Enter number: "))
+    c = int(input("Enter number: "))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg()
+
