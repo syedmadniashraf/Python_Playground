10ac1745d9513bb20f4d10bbb9c3ceca6ed6e1c0
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-29 23:18:08 +0530

    ch8

---
+++ chapter8/05_recursion.py
@@ -0,0 +1,11 @@
+'''
+
+factorial(1) = 1 
+factorial(2) = 1 x 2 
+factorial(3) = 1 x 2 x 3
+factorial(4) = 1 x 2 x 3 x 4 
+factorial(5) = 1 x 2 x 3 x 4 x 5
+factorial(n) = n x (n-1) x ....3 x 2 x 1
+
+factorial(n) = n * factorial(n-1)
+''' 
