49ee70625073e1ad71291f4d580e9517256e9ee2
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-06-30 23:29:27 +0530

    chapter 8 completed

--- chapter8/05_recursion.py
+++ chapter8/05_recursion.py
@@ -1,5 +1,6 @@
 '''
 
+factorial(0) = 1 
 factorial(1) = 1 
 factorial(2) = 1 x 2 
 factorial(3) = 1 x 2 x 3
@@ -9,3 +10,11 @@ factorial(n) = n x (n-1) x ....3 x 2 x 1
 
 factorial(n) = n * factorial(n-1)
 ''' 
+
+def factorial(n):
+    if(n==1 or n==0):
+        return 1
+    return n * factorial(n-1)
+
+n = int(input("Enter number: "))
+print(f"The factorial of this number is: {factorial(n)}")
