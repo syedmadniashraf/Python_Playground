391697800b6b829e739ca1c1bb0965402dde1247
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-07-02 23:21:55 +0530

    ch8 PS

---
+++ chapter8-PS/04_problem.py
@@ -0,0 +1,1 @@
+# 4.Write a recursive function to calculate the sum of first n natural numbers.
---
+++ chapter8-PS/05_problem.py
@@ -0,0 +1,4 @@
+# 5. Write a python function to print first n lines of the following pattern:
+# ***
+# ** - for n = 3
+# *
