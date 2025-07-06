82164aa49d9217246f6803c44e3957cfca8c1089
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-07-06 23:23:47 +0530

    project1

---
+++ project 1/main.py
@@ -0,0 +1,3 @@
+# We all have played snake, water gun game in our childhood. If you haven’t, google the
+# rules of this game and write a python program capable of playing this game with the
+# user.
