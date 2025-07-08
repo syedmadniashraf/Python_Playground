5baf51e2696cced32794847203c22de8273134c5
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-07-08 22:40:42 +0530

    chapter 9 completed

---
+++ chapter 9/05_with.py
@@ -0,0 +1,10 @@
+f = open("file.txt")
+print(f.read())
+f.close()
+
+# The same can be written using the statement like this:
+
+with open("file.txt") as f:
+    print(f.read())
+
+# You dont have to explicitly close the file
