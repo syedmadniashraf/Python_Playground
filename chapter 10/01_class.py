13edad669e538eba59cb10b2af8d8707e46b3b82
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-07-12 23:44:11 +0530

    Ch10

--- chapter 10/01_class.py
+++ chapter 10/01_class.py
@@ -1,1 +1,16 @@
-# class
+class Employee:
+    language = "Py" # This is a class attribute
+    salary = 1200000
+
+
+harry = Employee()
+harry.name = "Harry" # This is an instance attribute
+print(harry.name,harry.language,harry.salary)
+
+
+rohan = Employee()
+rohan.name = "Rohan Roro Robinson"
+print(rohan.name,rohan.language,rohan.salary)
+
+
+# Here name is instance attribute and salary and language are class attributes as they directly belong to the class
