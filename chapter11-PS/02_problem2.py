a4acfc6c2cb21a892acba01df61bbc51b564c83f
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-07-20 22:09:47 +0530

    ch11PS

--- chapter11-PS/02_problem2.py
+++ chapter11-PS/02_problem2.py
@@ -1,3 +1,2 @@
 # 2. Create a class ‘Pets’ from a class ‘Animals’ and further create a class ‘Dog’ from
 # ‘Pets’. Add a method ‘bark’ to class ‘Dog’
-# .
---
+++ chapter11-PS/03_problem3.py
@@ -0,0 +1,1 @@
+# 3. Create a class ‘Employee’ and add salary and increment properties to it.
