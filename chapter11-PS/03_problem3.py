e06ee5afbed7d102b157ce4121647628f3d10c57
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-07-21 23:33:16 +0530

    11 PS

--- chapter11-PS/03_problem3.py
+++ chapter11-PS/03_problem3.py
@@ -1,1 +1,3 @@
 # 3. Create a class ‘Employee’ and add salary and increment properties to it.
+# Write a method ‘salaryAfterIncrement’ method with a @property decorator with a setter
+# which changes the value of increment based on the salary.
