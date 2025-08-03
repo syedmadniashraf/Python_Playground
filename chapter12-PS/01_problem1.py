47caf9750f68dddf6d40ce25021ba2550481bf93
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-08-03 23:29:45 +0530

    12ps

--- chapter12-PS/01_problem1.py
+++ chapter12-PS/01_problem1.py
@@ -1,2 +1,5 @@
 # 1. Write a program to open three files 1.txt, 2.txt and 3.txt if any these files are not present, 
 # a message without exiting the program must be printed prompting the same.
+
+with open("1.txt", "r") as f:
+    print(f.read())
