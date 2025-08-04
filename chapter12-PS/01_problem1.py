0b01bb45bb3ed2cc18541a3a7a849b948afff0c2
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-08-04 22:26:55 +0530

    ch12-ps

--- chapter12-PS/01_problem1.py
+++ chapter12-PS/01_problem1.py
@@ -1,5 +1,23 @@
 # 1. Write a program to open three files 1.txt, 2.txt and 3.txt if any these files are not present, 
 # a message without exiting the program must be printed prompting the same.
 
-with open("1.txt", "r") as f:
-    print(f.read())
+try:
+    with open("1.txt", "r") as f:
+        print(f.read()) 
+except Exception as e:
+    print(e)
+
+try:
+    with open("2.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+try:
+    with open("3.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+    
+
+print("Thank you")
--- chapter12-PS/02_problem2.py
+++ chapter12-PS/02_problem2.py
@@ -1,1 +1,7 @@
 # 2. Write a program to print third, fifth and seventh element from a list using enumerate function.
+
+l = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i, item in enumerate(l):
+    if i == 2 or i == 4 or i == 6:
+        print(item)
