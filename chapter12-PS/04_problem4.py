c0fe964248c19a226cef32d503eda7ed5d58059b
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-08-05 23:42:42 +0530

    chapter12-PS

--- chapter12-PS/04_problem4.py
+++ chapter12-PS/04_problem4.py
@@ -1,2 +1,9 @@
 # 4. Write a program to display a/b where a and b are integers. If b=0, display infinite by
 #  handling the ‘ZeroDivisionError’.
+
+try:
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    print(a/b)
+except ZeroDivisionError as v:
+    print("Infinite")
--- chapter12-PS/05_problem5.py
+++ chapter12-PS/05_problem5.py
@@ -1,1 +1,8 @@
 # 5. Store the multiplication tables generated in problem 3 in a file named Tables.txt.
+
+n = int(input("Enter a number: "))
+
+table = [n*i for i in range(1, 11)]
+
+with open("tables.txt","a") as f:
+    f.write(f"Table of {n}: {str(table)} \n")
