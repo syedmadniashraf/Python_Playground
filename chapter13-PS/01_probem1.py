63d2c09652180eef49a0aed623eea185df292bee
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-08-10 23:08:16 +0530

    ch13ps

---
+++ chapter13-PS/01_probem1.py
@@ -0,0 +1,3 @@
+# 1. Create two virtual environments, install few packages in the first one. How do you create a similar 
+# environment in the second one?
+
---
+++ chapter13-PS/03_problem3.py
@@ -0,0 +1,2 @@
+# 3. A list contains the multiplication table of 7. write a program to convert it to vertical
+# string of same numbers.
