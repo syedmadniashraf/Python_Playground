f132e40092f2cb575e0026c82c9db5f222e687f1
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-08-15 22:57:17 +0530

    13.ps

--- chapter13-PS/04_problem4.py
+++ chapter13-PS/04_problem4.py
@@ -1,2 +1,11 @@
 # 4. Write a program to filter a list of numbers which are divisible by 5
 
+def divisible5(n):
+    if(n%5 == 0):
+        return True
+    return False
+
+a = [1, 2, 34234, 53, 6234235, 64343, 65, 754, 45, 55]
+
+f = list(filter(divisible5,a))
+print(f)
