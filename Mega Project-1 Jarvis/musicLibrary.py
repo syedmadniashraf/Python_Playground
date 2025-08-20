4ee171b36e23a0c52481b23ae5074deccd5b4246
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-08-20 23:15:52 +0530

    mega project

---
+++ Mega Project-1 Jarvis/musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "aadat": "https://www.youtube.com/watch?v=eK5gPcFjQps",
+    "chal diye tum kahan": "https://www.youtube.com/watch?v=4djVBhMrkzo",
+    "skyfall": "https://www.youtube.com/watch?v=sZrTJesvJeo",
+    "maria": "https://www.youtube.com/watch?v=jH4_GTRUC14",
+    "ek": "https://www.youtube.com/watch?v=mfX7ynqrq2k"
+}
