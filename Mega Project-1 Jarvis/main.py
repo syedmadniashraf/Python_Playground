352502e480c26df25836b4a965c0fce72ca7a4c3
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-08-22 23:40:05 +0530

    project

--- Mega Project-1 Jarvis/main.py
+++ Mega Project-1 Jarvis/main.py
@@ -9,7 +9,7 @@ import platform
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
-newsapi = "daa4892f639b4c36996449404766324e"
+newsapi = ""
 
 def speak(text):
     print(f"Speaking: {text}")
