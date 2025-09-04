41569c9d2b55a31053fc41b5ded19f7d55e624f1
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-09-04 23:52:37 +0530

    revision

--- REVISION/01_problem.py
+++ REVISION/01_problem.py
@@ -1,2 +1,16 @@
-print("hello world")
-print("I am good")
+# 1. Write a program to print Twinkle twinkle little star poem in python. 
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are,
+Up above the world so high,
+Like a diamond in the sky.
+
+When the traveler in the dark
+Thanks you for thy tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+When the blazing sun is gone,
+And he nothing shines upon,
+Then appears thy tiny spark,
+Twinkle, twinkle, in the dark.''')
