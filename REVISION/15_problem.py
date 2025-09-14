35fe357346838acefe3b5a2ed9cea0fc19c5017e
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-09-14 23:09:41 +0530

    revision

--- REVISION/15_problem.py
+++ REVISION/15_problem.py
@@ -1,4 +1,4 @@
 # 15. Replace the double space from problem 14 with single spaces. 
-name = "My   name   is Syed Madni"
+name = "My   name   is Syed Madni Ashraf"
 print(name.replace("  ", " ")) # strings are immutable, so no change
 # print(name)
