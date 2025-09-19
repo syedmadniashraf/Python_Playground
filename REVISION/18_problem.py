9d6991be2bf71630cdc0c00aa1e8b86f92878bde
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-09-19 23:00:02 +0530

    revision

---
+++ REVISION/18_problem.py
@@ -0,0 +1,4 @@
+# 18. Check that a tuple type cannot be changed in python. 
+a = ("Madni", "uttarakhand", "black", 14)
+a[0] = "Syed"
+print(a)
---
+++ REVISION/19_problem.py
@@ -0,0 +1,3 @@
+# 19. Write a program to sum a list with 4 numbers. 
+list = [1, 5, 7, 7]
+print(sum(list))
