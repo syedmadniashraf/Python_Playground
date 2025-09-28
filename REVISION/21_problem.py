a42b24f662acbced06fefaa45de15d7e31183e03
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2025-09-28 23:14:08 +0530

    revision

---
+++ REVISION/21_problem.py
@@ -0,0 +1,10 @@
+#21. Write a program to create a dictionary of Hindi words with values as their English 
+# translation. Provide user with an option to look it up! 
+dict1 = {
+    "key": "Value",
+    "chair": "kursi",
+    "lock": "Tala",
+    "book": "kitab"
+}
+dict2 = input("Enter the word you want meaning of: ")
+print(dict1[dict2])
